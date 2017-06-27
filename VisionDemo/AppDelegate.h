6c4df43948596c60132921af6f9a5fcf1ba8afcf
高永祥 <elvisgao@elvisgao-mb1.local>
2017-06-27 19:58:49 +0800

    Initial Commit

---
+++ VisionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VisionDemo
+//
+//  Created by 高永祥 on 2017/6/27.
+//  Copyright © 2017年 高永祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VisionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VisionDemo
+//
+//  Created by 高永祥 on 2017/6/27.
+//  Copyright © 2017年 高永祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
